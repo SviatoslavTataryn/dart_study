cb5d64486c981222c4849270a93595fd67efb1e3
sviatoslavtataryn <www.t.svyatoslav55@gmail.com>
2025-02-26 20:18:10 +0100

    First task Completed

---
+++ lib/Lesson1Solution.dart
@@ -0,0 +1,13 @@
+void main() {
+  String firstName = "Sviatoslav";
+  String lastName = "Tataryn";
+  int age = 27;
+  double temperature = 36.6;
+  bool lovesCoding = true;
+
+  print("name: $firstName");
+  print("last name: $lastName");
+  print("age: $age");
+  print("temperature body: $temperature");
+  print("or like i programing: $lovesCoding");
+}
